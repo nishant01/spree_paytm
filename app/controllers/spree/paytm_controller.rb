279e371623016de2fd050b0a041ce028c8200240
Nishant <nishant01@gmail.com>
2017-08-03 18:28:18 +0530

    Update paytm_controller.rb

--- app/controllers/spree/paytm_controller.rb
+++ app/controllers/spree/paytm_controller.rb
@@ -1,10 +1,10 @@
 module Spree
-  class PaytmController < StoreController
+  class PaytmController < Spree::StoreController
     protect_from_forgery only: :index
-    
+
     def index
       payment_method = Spree::PaymentMethod.find(params[:payment_method_id])
-      order = current_order
+      order = Spree::Order.find_by_number!(params[:order])
       @param_list = Hash.new
       @param_list['MID'] = payment_method.preferred_merchant_id
       @param_list['INDUSTRY_TYPE_ID'] = payment_method.preferred_industry_type_id
@@ -13,8 +13,9 @@ module Spree
       @param_list['REQUEST_TYPE'] = payment_method.request_type
       @param_list['ORDER_ID'] = payment_method.txnid(order)
       @param_list['TXN_AMOUNT'] = order.total.to_s
+      @param_list['CALLBACK_URL'] = PAYTM_CALLBACK_URL
 
-      if(address = current_order.bill_address || current_order.ship_address)
+      if(address = order.bill_address || order.ship_address)
         phone = address.phone
       end
       #if user is not loggedin, Passing phone as customer id
@@ -29,6 +30,31 @@ module Spree
     end
 
     def confirm
+      paytmparams = Hash.new
+      keys = params.keys
+      keys.each do |k|
+        paytmparams[k] = params[k]
+      end
+
+      paytmparams.delete("CHECKSUMHASH")
+      paytmparams.delete("controller")
+      paytmparams.delete("action")
+
+      @order = Spree::Order.find_by_number!(paytmparams["ORDERID"])
+      if paytmparams["STATUS"] == "TXN_SUCCESS"
+        flash.notice = Spree.t(:order_processed_successfully)
+        flash['order_completed'] = true
+        @order.complete
+        redirect_to completion_route
+      else
+        @order.next
+        redirect_to checkout_state_path(@order.state)
+      end
+    end
+
+    private
+    def completion_route
+      spree.order_path(@order)
     end
   end
 end
